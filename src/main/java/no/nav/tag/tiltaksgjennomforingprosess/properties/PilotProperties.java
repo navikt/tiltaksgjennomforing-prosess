640799746e537bdd4f61d45808fe3c1c435cb627
Sindre Dahl Løken <sindre.loken@nav.no>
2022-05-23 14:10:00 +0200

    initialiserer pilotvirksomheter til new arraylist som default så den alltid er tom hvis den ikke har fått noen miljøvariabler injectet.
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>
    Co-authored-by: Mattias Lundmark <mattias.lundmark@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforingprosess/properties/PilotProperties.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforingprosess/properties/PilotProperties.java
@@ -4,11 +4,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "prosess.pilot")
 public class PilotProperties {
-    private List<String> pilotvirksomheter;
+    private List<String> pilotvirksomheter = new ArrayList<>();
 }
