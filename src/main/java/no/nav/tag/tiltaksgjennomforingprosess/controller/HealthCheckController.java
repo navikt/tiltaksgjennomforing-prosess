c59745831a22374c4f1fdad72efb79885a61d976
d142796 <millad.dagdoni@nav.no>
2025-03-19 15:19:30 +0100

    logging av feil når vi får 500 på ping til tiltaksgjennomforing api
    Co-authored-by: Odd Andreas Sørsæther <oddsor@users.noreply.github.com>

--- src/main/java/no/nav/tag/tiltaksgjennomforingprosess/controller/HealthCheckController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforingprosess/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package no.nav.tag.tiltaksgjennomforingprosess.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.tag.tiltaksgjennomforingprosess.properties.TiltakApiProperties;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import java.net.URI;
 
 @RestController
 @Unprotected
+@Slf4j
 public class HealthCheckController {
 
     private final RestTemplate restTemplate;
@@ -36,6 +38,7 @@ public class HealthCheckController {
         try {
             ping = restTemplate.getForObject(uri, String.class);
         } catch (Throwable t) {
+            log.error("PING KALL TIL TILTAKSGJENNOMFORING FEILET",t);
             throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, API_FEIL);
         }
 
