22cdf01785730715bfe2b6f2923d43ae9950c97d
Magnus Rom <magnus.skalleberg.rom@nav.no>
2025-03-28 12:10:29 +0100

    Lagt til fiks

--- src/main/java/no/nav/tag/tiltaksgjennomforingprosess/persondata/PersondataService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforingprosess/persondata/PersondataService.java
@@ -3,6 +3,7 @@ package no.nav.tag.tiltaksgjennomforingprosess.persondata;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -18,6 +19,10 @@ public class PersondataService {
     }
 
     public Map<String, Diskresjonskode> hentDiskresjonskoder(Set<String> fnrSet) {
+        if(fnrSet.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
         if (fnrSet.size() > 1000) {
             throw new IllegalArgumentException("Kan ikke hente diskresjonkode for mer enn 1000 om gangen");
         }
